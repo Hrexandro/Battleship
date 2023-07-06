91b082f23108890b52854552f19615a05bbd7529
Hrexandro <marcin_nowak_hrx@o2.pl>
2023-07-06 21:40:52 +0200

    preliminary setup for gameboard

--- battleship.js
+++ battleship.js
@@ -1,3 +1,12 @@
+// Create Gameboard factory.
+// Note that we have not yet created any User Interface. We should know our code is coming together by running the tests.
+// You shouldn’t be relying on console.log or DOM methods to make sure your code is doing what you expect it to.
+// Gameboards should be able to place ships at specific coordinates by calling the ship factory function.
+// Gameboards should have a receiveAttack function that takes a pair of coordinates, determines whether or
+// not the attack hit a ship and then sends the ‘hit’ function to the correct ship, or records the coordinates of the missed shot.
+// Gameboards should keep track of missed attacks so they can display them properly.
+// Gameboards should be able to report whether or not all of their ships have been sunk.
+
 function Ship(length = 1){
     return {
         length,
@@ -15,4 +24,8 @@ function Ship(length = 1){
     }
 }
 
-module.exports = { Ship };
+function Gameboard(){
+    return {}
+}
+
+module.exports = { Ship, Gameboard };
--- battleship.test.js
+++ battleship.test.js
@@ -24,3 +24,19 @@ test('bigger ship survives one git', () => {
   expect(newShip.hits).toBe(1);
   expect(newShip.sunk).toBe(false)
 });
+
+test('imports the Gameboard',()=>{
+  expect(typeof battleship.Gameboard()).toEqual('object')
+})
+
+//to test Gameboard
+//should be a board of coordinates
+
+//should be able to place ships at specific coordinates by calling the ship factory function.
+
+// Gameboards should have a receiveAttack function that takes a pair of coordinates, determines whether or
+// not the attack hit a ship and then sends the ‘hit’ function to the correct ship, or records the coordinates of the missed shot.
+
+// Gameboards should keep track of missed attacks so they can display them properly.
+
+// Gameboards should be able to report whether or not all of their ships have been sunk.
